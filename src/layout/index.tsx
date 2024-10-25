02915fd88d9d7c62c6eb4399b2339e799e102fe1
JiuRanYa <1084350607@qq.com>
2024-10-25 10:10:45 +0800

    feat: layout border

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,7 +4,7 @@ import Header from './Header';
 
 const Layout: React.FC = () => {
     return (
-        <div className="flex flex-col min-h-screen rounded-lg overflow-hidden shadow-lg">
+        <div className="flex flex-col min-h-screen rounded-lg overflow-hidden shadow-lg border">
             <Header />
             <div className="flex-grow p-8">
                 <Outlet />
